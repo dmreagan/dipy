40e0a6476a9b50d956cde6dfe7a325cb0d057055
David Reagan <dmreagan@iu.edu>
2018-06-09 18:34:20 -0400

    Replicate surface color with frag shader

--- doc/examples/viz_surfaces.py
+++ doc/examples/viz_surfaces.py
@@ -57,7 +57,7 @@ my_triangles = np.array([[0,  6,  4],
                          [0,  4,  5],
                          [0,  5,  1],
                          [1,  5,  7],
-                         [1,  7,  3]],dtype='i8')
+                         [1,  7,  3]], dtype='i8')
 
 
 """
@@ -71,15 +71,16 @@ ut_vtk.set_polydata_triangles(my_polydata, my_triangles)
 Save the ``vtkPolyData``
 """
 
-file_name = "my_cube.vtk"
-io_vtk.save_polydata(my_polydata, file_name)
-print("Surface saved in " + file_name)
+# file_name = "my_cube.vtk"
+# io_vtk.save_polydata(my_polydata, file_name)
+# print("Surface saved in " + file_name)
 
 """
 Load the ``vtkPolyData``
 """
 
-cube_polydata = io_vtk.load_polydata(file_name)
+# cube_polydata = io_vtk.load_polydata(file_name)
+cube_polydata = my_polydata
 
 """
 add color based on vertices position
@@ -92,12 +93,61 @@ ut_vtk.set_polydata_colors(cube_polydata, colors)
 print("new surface colors")
 print(ut_vtk.get_polydata_colors(cube_polydata))
 
+
+# set up mapper for custom shaders
+mapper = ut_vtk.get_polymapper_from_polydata(cube_polydata)
+
+# send color values to shaders
+mapper.MapDataArrayToVertexAttribute(
+    "color",
+    mapper.GetInput().GetPointData().GetScalars().GetName(),
+    vtk.vtkDataObject.FIELD_ASSOCIATION_POINTS,
+    -1
+)
+
+
+mapper.AddShaderReplacement(
+    vtk.vtkShader.Vertex,
+    "//VTK::Normal::Dec",  # replace the normal block
+    True,  # before the standard replacements
+    "//VTK::Normal::Dec\n"  # we still want the default
+    "in vec3 color;\n"  # get vertex attribute
+    "out vec3 colorVSOutput;\n",  # output to frag shader
+    False  # only do it once
+)
+mapper.AddShaderReplacement(
+    vtk.vtkShader.Vertex,
+    "//VTK::Normal::Impl",  # replace the normal block
+    True,  # before the standard replacements
+    "//VTK::Normal::Impl\n"  # we still want the default
+    "colorVSOutput = color;\n",  # pass through attribute
+    False  # only do it once
+)
+# now modify the fragment shader
+mapper.AddShaderReplacement(
+    vtk.vtkShader.Fragment,   # in the fragment shader
+    "//VTK::Normal::Dec",  # replace the normal block
+    True,  # before the standard replacements
+    "//VTK::Normal::Dec\n"  # we still want the default
+    "in vec3 colorVSOutput;\n",  # get color
+    False  # only do it once
+)
+mapper.AddShaderReplacement(
+    vtk.vtkShader.Fragment,   # in the fragment shader
+    "//VTK::Normal::Impl",  # replace the normal block
+    True,  # before the standard replacements
+    "//VTK::Normal::Impl\n"  # we still want the default calc
+    "diffuseColor = colorVSOutput / 255;\n",  # convert from [0, 255] -> [0,1]
+    False  # only do it once
+)
+
 """
 Visualize surfaces
 """
 
 # get vtkActor
 cube_actor = ut_vtk.get_actor_from_polydata(cube_polydata)
+cube_actor.SetMapper(mapper)
 
 # renderer and scene
 renderer = window.Renderer()
@@ -106,8 +156,8 @@ renderer.set_camera(position=(10, 5, 7), focal_point=(0.5, 0.5, 0.5))
 renderer.zoom(3)
 
 # display
-# window.show(renderer, size=(600, 600), reset_camera=False)
-window.record(renderer, out_path='cube.png', size=(600, 600))
+window.show(renderer, size=(600, 600), reset_camera=False)
+# window.record(renderer, out_path='cube.png', size=(600, 600))
 
 """
 .. figure:: cube.png
